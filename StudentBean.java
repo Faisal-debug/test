e431f117c27dd7eb5f50eeb9e12c474136675b51
faishal <faisal3000khan@gmail.com>
2020-02-18 13:18:56 +0530

    first commit for student

---
+++ StudentBean.java
@@ -0,0 +1,40 @@
+package com.jkt.ServletDemo;
+
+public class StudentBean implements java.io.Serializable 
+{
+	private String Firstname=null;
+	private String Lastname=null;
+	private int age=0;
+	
+	public StudentBean()
+	{
+		super();
+	}
+	
+	public void setFirstname(String Firstname)
+	{
+		this.Firstname=Firstname;
+	}
+	public String getFirstname()
+	{
+		return Firstname;
+	}
+	
+	public void setLastname(String Lastname)
+	{
+		this.Lastname=Lastname;
+	}
+	public String getLastname()
+	{
+		return Lastname;
+	}
+	public void setAge(int age)
+	{
+		this.age=age;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+
+}
